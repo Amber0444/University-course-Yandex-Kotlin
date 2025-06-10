bdb330b1ac24384f4ffaefc28f22c9c9fbc54e8a
Amber0444 <alexander.shevelev04@gmail.com>
2025-06-10 16:35:45 +0500

    created a builder for Note

--- app/src/main/java/com/example/notes/Note.kt
+++ app/src/main/java/com/example/notes/Note.kt
@@ -2,13 +2,40 @@ package com.example.notes
 
 import android.graphics.Color
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
 
-data class Note constructor(
+
+data class Note(
     val uuid: UUID,
     val title: String,
     val content: String,
-    val color: Color,
+    val color: Int,
     val importance: Importance,
-    val date: LocalDateTime,
-)
+    val dateOfDelete: LocalDateTime,
+) {
+    companion object {
+        fun build(block: Note.NoteBuilder.() -> Unit): Note =
+            Note.NoteBuilder().apply(block).build()
+    }
+
+    class NoteBuilder {
+        lateinit var title: String
+        lateinit var content: String
+        var color: Int = Color.WHITE;
+        var importance: Importance = Importance.COMMON
+
+        fun build(): Note {
+            requireNotNull(title) { "Title is required!" }
+            requireNotNull(content) { "Content is required!" }
+
+            return Note(
+                UUID.randomUUID(),
+                title,
+                content,
+                color,
+                importance,
+                LocalDateTime.now().plusDays(3)
+            )
+        }
+    }
+}
