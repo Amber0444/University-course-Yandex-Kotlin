6a24224125b64505b121dd3bef663e50f9936334
Amber0444 <alexander.shevelev04@gmail.com>
2025-06-10 21:25:27 +0500

    created a json parser for Note

--- app/src/main/java/com/example/notes/Note.kt
+++ app/src/main/java/com/example/notes/Note.kt
@@ -1,6 +1,7 @@
 package com.example.notes
 
 import android.graphics.Color
+import com.google.gson.JsonObject
 import java.time.LocalDateTime
 import java.util.*
 
@@ -14,27 +15,66 @@ data class Note(
     val dateOfDelete: LocalDateTime,
 ) {
     companion object {
+        fun Note.parseJsonObject(jsonObject: JsonObject): Note {
+            val parsedUuid: UUID = UUID.fromString(jsonObject.get("uuid").asString)
+            val parsedTitle: String = jsonObject.get("title").asString
+            val parsedContent: String = jsonObject.get("content").asString
+            val parsedColor = getColorFromJsonObject(jsonObject)
+            val parsedImportance = getImportanceFromJsonObject(jsonObject)
+            val parsedDateOfDelete: LocalDateTime = LocalDateTime.parse(jsonObject.get("date").asString)
+
+            return Note.build {
+                uuid = parsedUuid
+                title = parsedTitle
+                content = parsedContent
+                color = parsedColor
+                importance = parsedImportance
+                dateOfDelete = parsedDateOfDelete
+            }
+        }
+
+        private fun getColorFromJsonObject(jsonObject: JsonObject): Int {
+            var color = Color.WHITE
+            if (jsonObject.has("color")) {
+                val argbList: List<String> = jsonObject.get("color").asString.split(",")
+
+                color = Color.argb(argbList[0].toInt(), argbList[1].toInt(),
+                    argbList[2].toInt(), argbList[3].toInt())
+            }
+            return color
+        }
+
+        private fun getImportanceFromJsonObject(jsonObject: JsonObject): Importance {
+            var importance = Importance.COMMON
+            if (jsonObject.has("importance")) {
+                importance = Importance.valueOf(jsonObject.get("importance").asString)
+            }
+            return importance
+        }
+
         fun build(block: Note.NoteBuilder.() -> Unit): Note =
             Note.NoteBuilder().apply(block).build()
     }
 
     class NoteBuilder {
+        var uuid: UUID = UUID.randomUUID()
         lateinit var title: String
         lateinit var content: String
         var color: Int = Color.WHITE;
         var importance: Importance = Importance.COMMON
+        var dateOfDelete: LocalDateTime = LocalDateTime.now().plusDays(3)
 
         fun build(): Note {
             requireNotNull(title) { "Title is required!" }
             requireNotNull(content) { "Content is required!" }
 
             return Note(
-                UUID.randomUUID(),
+                uuid,
                 title,
                 content,
                 color,
                 importance,
-                LocalDateTime.now().plusDays(3)
+                dateOfDelete,
             )
         }
     }
